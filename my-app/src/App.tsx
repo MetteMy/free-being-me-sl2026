4bdb2560ccd9d4cfd0a520562637250570d73c70
My <mmga@itu.dk>
2026-06-13 21:35:57 +0200

    language switcher

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -6,6 +6,7 @@ import boopSfx from '../sounds/Grumpy ghost.m4a';
 
 import React from 'react';
 import { Quiz, type Question } from './QuizComponent';
+import { useTranslation } from 'react-i18next';
 
 const myQuestions: Question[] = [
   {
@@ -18,14 +19,32 @@ const myQuestions: Question[] = [
   // Add more questions here...
 ];
 
+export function LanguageChanger() {
+  const { t, i18n } = useTranslation();
+  const changeLanguage = (lng: string) => {
+    i18n.changeLanguage(lng);
+  };
+  return (
+    <div>
+    
+      <button onClick={() => changeLanguage("en")}>English</button>
+      <button onClick={() => changeLanguage("da")}>Danish</button>
+    </div>
+  );
+}
+
 const App = () => {
+  const { t, i18n } = useTranslation();
+  
   const handleQuizComplete = (finalScore: number) => {
     console.log(`Quiz Done! Total Score: ${finalScore}`);
   };
 
   return (
     <div className="app">
-      <h1>Quiz</h1>
+
+      <LanguageChanger></LanguageChanger>
+      <h1>{t("title")}</h1>
       <Quiz questions={myQuestions} onQuizComplete={handleQuizComplete} />
     </div>
   );
